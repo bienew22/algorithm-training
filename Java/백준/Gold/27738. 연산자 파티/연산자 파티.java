d413d690327a0c913cb9d74e2b4d227ffbdf9b25
HakRyong An <123283084+bienew22@users.noreply.github.com>
2026-01-08 17:41:33 +0900

    [Gold V] Title: 연산자 파티, Time: 232 ms, Memory: 17800 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Gold/27738. 연산자 파티/연산자 파티.java
+++ Java/백준/Gold/27738. 연산자 파티/연산자 파티.java
@@ -4,6 +4,8 @@
  * i가 F의 배수가 되면 결국 X은 0으로 초기화 되는 것을 의미합니다.
  * <p>
  * -- 해결 로그 --
+ * 10점 : for(;start <= N; start++) 대신 -> 힙을 이용하여 처리
+ * 10점 : 성능문제가 아니라 런타임에러 이라..
  */
 
 import java.util.*;
@@ -14,18 +16,15 @@ class Main {
 
     void solve() throws Exception {
         // 변수 초기화
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
 
-        long N = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        long C = Long.parseLong(st.nextToken());
-        long D = Long.parseLong(st.nextToken());
-        long E = Long.parseLong(st.nextToken());
-        long F = Long.parseLong(st.nextToken());
+        long N = sc.nextLong();
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        long D = sc.nextLong();
+        long E = sc.nextLong();
+        long F = sc.nextLong();
 
         long start = (N / F) * F + 1;
         long X = 0;
@@ -45,9 +44,6 @@ class Main {
             }
             if (start % E == 0) {
                 X |= start;
-            }
-            if (start % F == 0) {
-                X = 0;
             }
         }
 
