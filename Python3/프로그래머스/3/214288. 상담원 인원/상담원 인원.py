3475bcaeda3e5f16ffb05330d8cf3b276310c756
HakRyong An <123283084+bienew22@users.noreply.github.com>
2026-03-24 18:59:05 +0900

    [level 3] Title: 상담원 인원, Time: 3.06 ms, Memory: 9.22 MB -BaekjoonHub

--- Python3/프로그래머스/3/214288. 상담원 인원/상담원 인원.py
+++ Python3/프로그래머스/3/214288. 상담원 인원/상담원 인원.py
@@ -1,14 +1,10 @@
 from heapq import heappush, heappop
 from collections import defaultdict
 
+
 def solution(k, n, reqs):
-    # k : 상담 유형 수
-    # n : 멘토의 수 
-    # reqs : 상담 요청, [a, b, c], a 기준으로 오름 차순으로 정리 됨.
-    #   a - 상담 시작 시간
-    #   b - 상담 시간, 종료 시간 = a + b
-    #   c - 상담 유형
-    answer = 10 ** 10
+    
+    answer = 0
     
     # 상담 유형 별로 분리
     map_k_reqs = defaultdict(list)
@@ -16,44 +12,51 @@ def solution(k, n, reqs):
     for req in reqs:
         map_k_reqs[req[2] - 1].append([req[0], req[1]])
 
+    # 멘토를 추가할 때 감소되는 대기 시간을 나타냄.
+    # diff_heap[i] = [a, b, c, d]
+    #   - a : 감소되는 대기 시간량
+    #   - b : 상담 요청 타입,
+    #   - c : 멘토 인원 수
+    #   - d : 대기 시간.
+    diff_heap = []
     
-    # DP[k][i] = k 유형 상담에 i 명의 상담사 있을 때 대기 시간
-    DP = {_: [0] * (n - k + 2)  for _ in range(k)}
-    
-    for now_n in range(1, n - k + 2):
+    for now_k in map_k_reqs:
+        # 멘토 1명인 경우
+        wait_time1 = get_wait_time(1, map_k_reqs[now_k])
         
-        for now_k in map_k_reqs:
-            counselors = [0] * now_n
-            wait_time = 0
-            
-            for req in map_k_reqs[now_k]:
-                last_time = heappop(counselors)
-                
-                if req[0] >= last_time:
-                    heappush(counselors, req[0] + req[1])
-                else:
-                    wait_time += last_time - req[0]
-                    heappush(counselors, last_time + req[1])
-            
-            DP[now_k][now_n] = wait_time
-                
-    
-    for counselors in get_counselors([1] * k, n - k, 0):
+        # 멘토 2명인 경우
+        wait_time2 = get_wait_time(2, map_k_reqs[now_k])
         
-        wait = sum(DP[k_][c_] for k_, c_ in enumerate(counselors))
+        answer += wait_time1
         
-        answer = min(answer, wait)
+        diff = [wait_time2 - wait_time1, now_k, 2, wait_time2]
         
+        heappush(diff_heap, diff)
     
-    # print(DP)
-    
+    for i in range(n - k):
+        now = heappop(diff_heap)
+        
+        answer += now[0]
+        
+        wait_time = get_wait_time(now[2] + 1, map_k_reqs[now[1]])
+        
+        next_diff = [wait_time - now[3], now[1], now[2] + 1, wait_time]
+        
+        heappush(diff_heap, next_diff)
+        
     return answer
 
-def get_counselors(arr, n, idx):
-    if n == 0:
-        yield arr
-    else:
-        for i in range(idx, len(arr)):
-            arr[i] += 1
-            yield from get_counselors(arr, n - 1, i)
-            arr[i] -= 1
+def get_wait_time(mentor_cnt, reqs):
+    mentors = [0] * mentor_cnt
+    wait_time = 0
+    
+    for req in reqs:
+        last_time = heappop(mentors)
+        
+        if req[0] >= last_time:
+            heappush(mentors, req[0] + req[1])
+        else:
+            wait_time += last_time - req[0]
+            heappush(mentors, last_time + req[1])
+    
+    return wait_time
