c23523d88837ed118fb45ab1b7815932f501ca50
HakRyong An <123283084+bienew22@users.noreply.github.com>
2026-05-07 16:08:17 +0900

    [level 3] Title: 억억단을 외우자, Time: 384.73 ms, Memory: 280 MB -BaekjoonHub

--- Java/프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ Java/프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -1,9 +1,75 @@
+import java.util.*;
+
 class Solution {
+    public int[] numOfDivisors(int num) {
+        ArrayList<Integer> primes = new ArrayList<>();
+
+        int[] spf = new int[num + 1];   // 최소 소인수
+        int[] exp = new int[num + 1];   // 최소 소인수의 지수
+        int[] div = new int[num + 1];   // 약수 개수
+
+        div[1] = 1;
+
+
+        for (int n = 2; n <= num; n++) {
+
+            // 소수인 경우
+            if (spf[n] == 0) {
+                spf[n] = n;
+                exp[n] = 1;
+
+                // 소수의 약수 개수는 2개
+                div[n] = 2;
+
+                primes.add(n);
+            }
+
+            for (int prime : primes) {
+                int composite = prime * n;
+
+                if (composite > num) {
+                    break;
+                }
+
+                spf[composite] = prime;
+
+                // prime이 n의 최소 소인수인 경우
+                if (n % prime == 0) {
+
+                    // exponent 증가
+                    exp[composite] = exp[n] + 1;
+
+                    /*
+                     * n = m × prime^k
+                     *
+                     * d(n) = d(m) × (k + 1)
+                     *
+                     * composite = m × prime^(k + 1)
+                     *
+                     * d(composite)
+                     * = d(m) × (k + 2)
+                     */
+                    div[composite] = div[n] / (exp[n] + 1) * (exp[n] + 2);
+
+                    break;
+                }
+
+                // 새로운 소인수가 추가되는 경우
+                exp[composite] = 1;
+
+                // 약수 개수 2배
+                div[composite] = div[n] * 2;
+            }
+        }
+
+        return div;
+    }
+    
     public int[] solution(int e, int[] starts) {
         int[] answer = new int[starts.length];
 
         // 각 수의 약수의 개수
-        int[] divisors = new int[e + 1];
+        int[] divisors = numOfDivisors(e);
 
         // dp[i]: i ~ e 사이 수 중에서 약수가 가장 많은수
         int[] dp = new int[e + 1];
